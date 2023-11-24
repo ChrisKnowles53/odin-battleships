bda21155c87a067c501d81489d74279365e7fb19
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-11-24 16:06:37 +0000

    getting rid of console.logs and writing tests

--- src/index.js
+++ src/index.js
@@ -10,15 +10,3 @@ console.log(firstShip.hit, firstShip.sunk);
 
 firstShip.numberOfTimesHit(1);
 console.log(firstShip.hit, firstShip.sunk);
-
-let board = new Gameboard();
-console.log(board);
-
-let row = 0; // expect A as the answer
-console.log(String.fromCharCode(65 + row));
-let row2 = 1; // remember arrays start at 0 expect B as the answer
-console.log(String.fromCharCode(65 + row2));
-
-board.setShipStartPosition("1", "0", "test");
-console.log(board);
-board.setShipStartPosition("5", "5", "test");
