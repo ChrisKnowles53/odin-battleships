8d0371ef5a63818bd80a65b42702378c7da810fb
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-11-29 10:51:39 +0000

    rename const board to gaemboard, added new tests, upadted gameboard to pass new tests

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -5,31 +5,21 @@ class Gameboard {
   }
   placeShip(locations, ship) {
     locations.forEach((location) => {
-      this.board[location] = ship;
+      // if isPlaceAvailble(locations) returns true place ship else retrun "choose another space"
+      if (this.board[location] === undefined) {
+        this.board[location] = ship;
+      } else {
+        console.log("Space occupied choose another space");
+      }
     });
   }
+  // isPlaceAvailable(locations) {
+  //     //code to check all spaces are free
+  //     // code to check if a particular cell is free first
+  //     // then check next cell
+  //     // this is starting to smell of multiple if statements (max ship is 5 cells)
+
+  // }
 }
 
 module.exports = Gameboard;
-
-//   initialiseBoard(size) {
-//     let board = [];
-//     for (let row = 0; row < size; row++) {
-//       board[row] = [];
-//       for (let column = 0; column < size; column++) {
-//         board[row][column] = this.convertRowLabel(row) + (column + 1);
-//       }
-//     }
-//     return board;
-//   }
-//   convertRowLabel(row) {
-//     return String.fromCharCode(65 + row); // CharCode: capital letters start at A is position 65 and finish at Z at position 90
-//   }
-
-//   setShipStartPosition(row, column, value) {
-//     // think if i limit input to between 1 and 10 i dont need this check
-//     //💥💥 This only works with row and column as digits starting at 0 for first column
-//     if (row < this.size && column < this.size) {
-//       this.board[row][column] = value;
-//     }
-//   }
--- src/classes/gameboard.test.js
+++ src/classes/gameboard.test.js
@@ -3,27 +3,74 @@ const Gameboard = require("./gameboard.js");
 
 test("placeShip function", () => {
   const ship1 = new CreateShip(1);
-  const board = new Gameboard();
-  board.placeShip(["a1"], ship1);
-  expect(board.board.a1).toBe(ship1);
+  const gameboard = new Gameboard();
+  gameboard.placeShip(["a1"], ship1);
+  expect(gameboard.board.a1).toBe(ship1);
 });
 
 test("placeship across 2 cells", () => {
   const ship2 = new CreateShip(2);
-  const board2 = new Gameboard();
+  const gameboard = new Gameboard();
   const locations = ["a3", "a4"];
-  board2.placeShip(locations, ship2);
-  expect(board2.board.a3).toBe(ship2);
-  expect(board2.board.a4).toBe(ship2);
+  gameboard.placeShip(locations, ship2);
+  expect(gameboard.board.a3).toBe(ship2);
+  expect(gameboard.board.a4).toBe(ship2);
 });
 
-// whats the next test
+// test validate can a single placed on top of another
+test("validate that a second ship cannot be placed on top of the first ship", () => {
+  const ship1 = new CreateShip(1);
+  const ship2 = new CreateShip(1);
+  const gameboard = new Gameboard();
+  gameboard.placeShip(["a1"], ship1);
+  expect(gameboard.board.a1).toBe(ship1);
+  gameboard.placeShip(["a1"], ship2);
+  expect(gameboard.board.a1).toBe(ship1);
+});
+
+// test validate can i place an exact overlap ship
+test("validate that a second ship of exact same length cannot be placed on top of the first ship", () => {
+  const ship1 = new CreateShip(2);
+  const ship2 = new CreateShip(2);
+  const gameboard = new Gameboard();
+  gameboard.placeShip(["a1", "b1"], ship1);
+  expect(gameboard.board.a1).toBe(ship1);
+  gameboard.placeShip(["a1", "b1"], ship2);
+  expect(gameboard.board.a1).toBe(ship1);
+});
+
+// test partial overlap
+// test("validate that a ship cannot partially overlap an exisitng ship", () => {
+//   const ship1 = new CreateShip(1);
+//   const ship2 = new CreateShip(2);
+//   const gameboard = new Gameboard();
+// });
+// test partial front and rear overlap
+
+// test("checks that a cell is empty before placing a ship", () => {
+//   const board3 = new Gameboard();
+//   const ship3 = new CreateShip(2);
+//   const ship4 = new CreateShip(2);
+//   const locations3 = ["a3", "a4"];
+//   const locations4 = ["a4", "a5"];
+
+//   board3.placeShip(locations3, ship3);
+//   expect(board3.board.a3).toBe(ship3);
+//   expect(board3.board.a4).toBe(ship3);
+
+//   board3.placeShip(locations3, ship4);
+//   expect(board3.board.a3).toBe(ship3);
+//   expect(board3.board.a4).toBe(ship3);
+
+//   // make this test first location is empty second location is populated
+//   board3.placeShip(locations4, ship4);
+//   expect(board3.board.a4).toBe(ship3);
+//   expect(board3.board.a5).toBe(undefined);
+// });
+
+// whats the next test - make sure the cell is empty before placing the ship
 
 // red green refactor - simple test fail write the code to pass write the next test fail and code to pass then refactor
-
-// ✨✨✨✨ write a test for placement of a ship on the board
-// is there another ship on the board - ie is the cell empty
-// are there enough rows/columns to fit the ship ??
 
 // cell states :  empty, ship identifier, hit, clicked but not a ship present
 //// empty: null
